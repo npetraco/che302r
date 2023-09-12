f4562cfee9b39086347f3d260a8c4a639a4ec13d
npetraco <npetraco@gmail.com>
2023-09-12 08:10:48 -0400

    Remote update

--- R/constants.R
+++ R/constants.R
@@ -1,5 +1,9 @@
-h   <- 6.626068e-34  # Planck's const
-cl  <- 299792458     # Speed of light
-kB  <- 1.3806503e-23 # Boltzmann's const
-hb  <- h/(2*pi)      # Reduced Planck's const
-N.A <- 6.02214076e23 # Avogadro's number
+h    <- 6.626068e-34    # Planck's const
+cl   <- 299792458       # Speed of light
+kB   <- 1.3806503e-23   # Boltzmann's const
+hb   <- h/(2*pi)        # Reduced Planck's const
+N.A  <- 6.02214076e23   # Avogadro's number
+
+# Handy Conversions
+eV2J <- 1.602176634e-19 # electron-volt to Joules, energy gained by 1 e- accelerated though 1V
+J2eV <- eV2J^(-1)       # Joules to eV
---
+++ R/util.R
@@ -0,0 +1,14 @@
+#' Easily update the che302r library by installing the current version from the github site
+#'
+#' Easily update the che302r library by installing the current version from the github site
+#'
+#' @param XX The XX
+#' @return The function will XX
+#'
+#'
+#' @export
+update_che302r <- function() {
+  print("Updating che302r")
+  devtools::install_github("npetraco/che302r")
+  print("Done!")
+}
