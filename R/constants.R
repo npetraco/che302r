36d8f845c18adee061b3d88a8e8d27f7c99bbd03
npetraco <npetraco@gmail.com>
2023-09-12 09:39:03 -0400

    Remote update

--- R/constants.R
+++ R/constants.R
@@ -1,9 +1,19 @@
-h    <- 6.626068e-34    # Planck's const
-cl   <- 299792458       # Speed of light
-kB   <- 1.3806503e-23   # Boltzmann's const
-hb   <- h/(2*pi)        # Reduced Planck's const
-N.A  <- 6.02214076e23   # Avogadro's number
+h    <- 6.626068e-34          # Planck's const
+cl   <- 299792458             # Speed of light
+kB   <- 1.3806503e-23         # Boltzmann's const
+hb   <- h/(2*pi)              # Reduced Planck's const
+N.A  <- 6.02214076e23         # Avogadro's number
+me   <- 9.1093837015e-31      # mass of electron
+ec   <- 1.60217663e-19        # charge of electron in Coulombs
+RH   <- 109625                # Rydberg constant in wavenumbers
+RHJ  <- h * cl/( 1/(RH*100) ) # Rydberg constant in Joules
 
 # Handy Conversions
-eV2J <- 1.602176634e-19 # electron-volt to Joules, energy gained by 1 e- accelerated though 1V
-J2eV <- eV2J^(-1)       # Joules to eV
+eV2J         <- 1.602176634e-19 # electron-volt to Joules, energy gained by 1 e- accelerated though 1V
+J2eV         <- eV2J^(-1)       # Joules to eV
+hartree2J    <- 4.3597482e-18                           # hartrees to Joules
+amu2kg       <- 1.6603145e-27                           # amu to kg
+bohr2m       <- 5.2918e-11                              # bohr to meters
+auf2Hz       <- sqrt(hartree2J * 1/(amu2kg * bohr2m^2)) # atomic units frequency to Hz, Unit= s^-1
+lambdaFw2icm <- auf2Hz/(2*pi*cl*100)                    # atomic units mass weighted Fw eigenvalues to nu-tilde = omega/(2*pi*c), Unit = cm^-1
+                                                        # lambdaF2icm ---> 5140.485 # Conversion factor from sqrt(hartree/amu-bohr^2) to cm^-1
--- R/polyatomic.harmonic.vibs.functions.R
+++ R/polyatomic.harmonic.vibs.functions.R
@@ -1,10 +1,3 @@
-hartree2J    <- 4.3597482e-18                           # hartrees to Joules
-amu2kg       <- 1.6603145e-27                           # amu to kg
-bohr2m       <- 5.2918e-11                              # bohr to meters
-auf2Hz       <- sqrt(hartree2J * 1/(amu2kg * bohr2m^2)) # atomic units frequency to Hz, Unit= s^-1
-lambdaFw2icm <- auf2Hz/(2*pi*cl*100)                    # atomic units mass weighted Fw eigenvalues to nu-tilde = omega/(2*pi*c), Unit = cm^-1
-                                                        #lambdaF2icm ---> 5140.485 # Conversion factor from sqrt(hartree/amu-bohr^2) to cm^-1
-
 #--------------------------------------------
 #' @title XXXX
 #' @description XXXX
