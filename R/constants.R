9ba5feb50e2b674b759076306419b1329744a84f
npetraco <npetraco@gmail.com>
2023-09-19 09:46:01 -0400

    Remote update

--- R/constants.R
+++ R/constants.R
@@ -1,16 +1,18 @@
-h    <- 6.626068e-34          # Planck's const
-cl   <- 299792458             # Speed of light
-kB   <- 1.3806503e-23         # Boltzmann's const
-hb   <- h/(2*pi)              # Reduced Planck's const
-N.A  <- 6.02214076e23         # Avogadro's number
-me   <- 9.1093837015e-31      # mass of electron
-ec   <- 1.60217663e-19        # charge of electron in Coulombs
-RH   <- 109625                # Rydberg constant in wavenumbers
-RHJ  <- h * cl/( 1/(RH*100) ) # Rydberg constant in Joules
+h    <- 6.626068e-34          # Planck's const, Js
+cl   <- 299792458             # Speed of light, m/s
+kB   <- 1.3806503e-23         # Boltzmann's const, J/K
+hb   <- h/(2*pi)              # Reduced Planck's const, Js
+N.A  <- 6.02214076e23         # Avogadro's number, particles
+me   <- 9.1093837015e-31      # mass of electron, kg
+mP   <- 1.67262192369e-27     # mass of proton, kg
+mN   <- 1.67492749804e-27     # mass of neutron, kg
+ec   <- 1.60217663e-19        # charge of electron, Coulombs
+RH   <- 109625                # Rydberg constant, wavenumbers cm^-1
+RHJ  <- h * cl/( 1/(RH*100) ) # Rydberg constant, Joules
 
 # Handy Conversions
-eV2J         <- 1.602176634e-19 # electron-volt to Joules, energy gained by 1 e- accelerated though 1V
-J2eV         <- eV2J^(-1)       # Joules to eV
+eV2J         <- 1.602176634e-19                         # electron-volt to Joules, energy gained by 1 e- accelerated though 1V
+J2eV         <- eV2J^(-1)                               # Joules to eV
 hartree2J    <- 4.3597482e-18                           # hartrees to Joules
 amu2kg       <- 1.6603145e-27                           # amu to kg
 bohr2m       <- 5.2918e-11                              # bohr to meters
